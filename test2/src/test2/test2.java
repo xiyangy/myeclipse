a8859a0157d8ad7ff851add44292f78517ba8b30
xiyang <454807130@qq.com>
2015-10-20 17:35:18 +0800

    特特色天使

---
+++ test2/src/test2/test2.java
@@ -0,0 +1,13 @@
+package test2;
+
+public class test2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("12333333333");
+	}
+
+}
