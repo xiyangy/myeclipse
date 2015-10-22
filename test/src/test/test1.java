ba6857c1c2802dd40e8228a3577b216fc3a7fe83
xiyang <454807130@qq.com>
2015-10-22 14:26:45 +0800

    asdfghjkl

--- test/src/test/test1.java
+++ test/src/test/test1.java
@@ -5,6 +5,5 @@ public static void main(String[] args) {
 	System.out.println("123333333333333");
 	System.out.println("123333333333333");
 	System.out.println("123333333333333");
-	System.out.println("123333333333333");
 }
 }
